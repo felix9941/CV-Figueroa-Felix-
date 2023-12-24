35d60776fa5da961212b1460dcc1fd30433e069b
Felix Figueroa <felixfigueroa41@gmail.com>
2023-12-24 01:43:55 -0300

    Se finalizo con el diseño, y se agrego animaciones con Css

--- JS/index.js
+++ JS/index.js
@@ -18,3 +18,83 @@ for (var x = 0; x < links.length; x++) {
     menu_visible = false;
   };
 }
+
+//Barritas
+function crearBarra(id_barra) {
+  for (i = 1; i <= 10; i++) {
+    let div = document.createElement("div");
+    div.className = "e";
+    id_barra.appendChild(div);
+  }
+}
+
+let html = document.getElementById("html");
+crearBarra(html);
+
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+
+let metAgil = document.getElementById("metAgil");
+crearBarra(metAgil);
+
+let office = document.getElementById("office");
+crearBarra(office);
+
+let documentacion = document.getElementById("documentacion");
+crearBarra(documentacion);
+
+let iso = document.getElementById("iso");
+crearBarra(iso);
+
+//porcentaje a mostrar de cada barrita
+let contadores = [-1, -1, -1, -1, -1, -1];
+let entro = false;
+
+//animaciones
+function efectoConocimientos() {
+  var conocimientos = document.getElementById("conocimientos");
+  var distancia_knowledge =
+    window.innerHeight - conocimientos.getBoundingClientRect().top;
+  if (distancia_knowledge >= 300 && entro == false) {
+    entro = true;
+
+    const intervalHtml = setInterval(function () {
+      pintarBarra(html, 7, 0, intervalHtml);
+    }, 100);
+
+    const intervalJavascript = setInterval(function () {
+      pintarBarra(javascript, 5, 1, intervalJavascript);
+    }, 100);
+
+    const intervalMetAgil = setInterval(function () {
+      pintarBarra(metAgil, 7, 2, intervalMetAgil);
+    }, 100);
+
+    const intervalOffice = setInterval(function () {
+      pintarBarra(office, 7, 3, intervalOffice);
+    }, 100);
+
+    const intervalDocumentacion = setInterval(function () {
+      pintarBarra(documentacion, 8, 4, intervalDocumentacion);
+    }, 100);
+
+    const intervalIso = setInterval(function () {
+      pintarBarra(iso, 6, 5, intervalIso);
+    }, 100);
+  }
+}
+//Lleno una barra con la cantidad inidcada
+function pintarBarra(id_barra, cantidad, indice, interval) {
+  contadores[indice]++;
+  x = contadores[indice];
+  if (x < cantidad) {
+    let elementos = id_barra.getElementsByClassName("e");
+    elementos[x].style.backgroundColor = "rgb(188, 112, 18)";
+  } else {
+    clearInterval(interval);
+  }
+}
+
+window.onscroll = function () {
+  efectoConocimientos();
+};
